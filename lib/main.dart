1a9712a40c3bdf9f4b1dfd2549eb885a92eb1eed
Basit Amin <basitamin308@gmail.com>
2020-03-30 20:32:08 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
+  void sound(int num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Expanded buildKey({int c, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          sound(c);
+        },
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text(
-            'XyloPhone'
-          ),
-        ),
         body: SafeArea(
-          child: Center(
-            child: Container(
-              child: FlatButton(
-                onPressed: (){
-                  final player=AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('click me'),
-              ),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(c: 1, color: Colors.red),
+              buildKey(c: 2, color: Colors.orange),
+              buildKey(c: 3, color: Colors.yellow),
+              buildKey(c: 4, color: Colors.green),
+              buildKey(c: 5, color: Colors.teal),
+              buildKey(c: 6, color: Colors.blue),
+              buildKey(c: 7, color: Colors.purple),
+            ],
           ),
         ),
       ),
